4e052d16176513f02ae6e50e8290f71ce874671f
kaktuz <120398316+evgenyaroviy@users.noreply.github.com>
2023-08-28 23:03:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './components/redux/store';
 import { ChakraProvider } from '@chakra-ui/react';
-//  basename="/goit-react-hw-08-phonebook"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <ChakraProvider>
           <App />
         </ChakraProvider>
